cb1e5267b30f6bd623e3e316374c262a41a1f507
MaksymTovstolis <85698971+MaksymTovstolis@users.noreply.github.com>
2021-09-28 09:16:41 +0300

    v3

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
-let hero = ['Ivan'];
-let native = ['York','Of'];
-let destination = ['Poltava','In'];
-let rainbow;
+// exercise 1
 
-rainbow = hero.concat(native.reverse(),destination.reverse());
-rainbow[0] = `Richard`;
-rainbow[rainbow.length-1] = `Vain`;
-rainbow.splice(3,0,'Gave','Battle');
+const randomNumbers =  [23,1,2,52,5,34,23,6,246,436];
+const primes = [];
 
-console.log(rainbow);
-
-let colors = [`red`,`orange`,`yellow`,`green`,`blue`,`indigo`,`violet`];
-
-debugger;
-for(let i=0; i<rainbow.length; i++){
-	document.write (`
-    <tr>
-        <th style= background: ${colors[i]}; border-radius: 100%; width: 50px; height: 50px;> </th>   
-    </tr>
-    <tr>
-        <th>${rainbow[i]}</th>
-    </tr>
-    `)
+for(let n = 0; n < randomNumbers.length; n++) {
+  const randomNumber = randomNumbers[n];
+  
+  let flag = true;
+  for (let i = 2; i < randomNumber; i++) {
+    if (randomNumber % i == 0) {
+      flag = false; 
+      break;
+    }
+  }
+  if(flag) primes.push(randomNumber);
 }
+
+let  primesMin = Math.min(...primes) ;
+let primesMax = Math.max(...primes);
+console.log(primesMax);
+console.log(primesMin);
+
+console.log(primes);
+
+// exercise 2
+
+let secondA = [3444, 45 , 521, 111, 87, 23, 480];
+let second B =[];
+
+ secondA_min = Math.min(...secondA);
+  secondA_max = Math.max(...secondA);
+
+console.log(secondA.indexOf(secondA_min));
+console.log(secondA.indexOf(secondA_max));
+
+for (n = 1; n < secondA.length-2; n++) {
+    secondB.push(secondA[n]);
+}
+console.log(secondB)
+
+// exercise 3
+
+let thirdA = [150, 293, 486, 439, 105, 50, 297]
+for(let n = 0; n < secondA.length; n++) {
+    thirdA_min = Math.min(...thirdA);
+    thirdA_max = Math.max(...thirdA);
+}
+console.log(thirdA.indexOf(thirdA_min));
+console.log(thirdA.indexOf(thirdA_max));
+
+[thirdA[2], thirdA[5]] = [thirdA[5], thirdA[2]];
+
+console.log(thirdA);
